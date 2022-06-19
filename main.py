72ed3cf1919ac5139f3b3943ac176ca5a51ec851
tonysum <tonysum33@gmail.com>
2022-06-19 23:24:42 +0800

    雙向

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+# from sympy import Point, Polygon, Ray
+import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sympy import Point, Polygon, Ray
-
-
+from shapely.geometry import Polygon, Point, MultiPoint,LineString
+from shapely import wkb
+from shapely import affinity, ops
 
 class RcMaterial:
     def __init__(self,fc, fy):
@@ -40,7 +42,7 @@ class Rebars:
         self.rebars = rebars
         self.lowest = self.__lowest()
         self.total_area = self.__total_area()
-        self.coords = self.__coords()
+        self.coordinate = self.__coordinate()
 
     def __lowest(self):
         coord_ymin = min(rebar["coord"][1] for rebar in self.rebars)
@@ -50,7 +52,7 @@ class Rebars:
         area = sum(rebar['area'] for rebar in self.rebars)
         return area
 
-    def __coords(self):
+    def __coordinate(self):
         return [rebar["coord"] for rebar in self.rebars]   
 
     def rA(barSize): 
@@ -95,29 +97,71 @@ class RectangleSec:
 
 
 
-# -----------------------------------------------------------
-# 斷面資料 unit:cm
-rec = RectangleSec(width=50, height=80)
 
-# -----------------------------------------------------------
-# 鋼筋資料
-# 以斷面形心為原點中心 
-rebars = []
-rebars.append({"area":4*Rebars.rA(29),"coord":(25,35)})
-rebars.append({"area":0*Rebars.rA(29),"coord":(25,0)})
-rebars.append({"area":4*Rebars.rA(29),"coord":(25,-35)})
-rbs = Rebars(rebars)
+def geometry_transform(geometry, x0, y0, angle_deg):
+    # Coordinate Transformation to Centroid Origin
+    geometry = affinity.translate(geometry, - x0, -y0)
+    # Coordinate Transformation to Angular Rotation
+    geometry = affinity.rotate(geometry, -angle_deg, origin=(0, 0))
+    return geometry
 
+def computed_capacity_force(poly, neutral_axis_depth):
 
-# -----------------------------------------------------------
-# 材料資料 unit:cm
-mat = RcMaterial(fc = 280, fy = 4200)
+    # concrete_force
+    #
+    # a = effective compressive block depth for ACI-318 concrete model
+    a = mat.beta1 * neutral_axis_depth
 
-# -----------------------------------------------------------
-# 外力資料 unit:t-m
-pu0 = 0.0
-mu0 = 1.0
+    poly_xmin = poly.bounds[0]
+    poly_ymin = poly.bounds[1]
+    poly_xmax = poly.bounds[2]
+    poly_ymax = poly.bounds[3]
 
+    cut_line = LineString([(poly_xmin, poly_ymax - a),
+                           (poly_xmax, poly_ymax - a)])
+
+    cut_polys = ops.split(poly, cut_line)
+    cut_poly0_ymax = cut_polys.geoms[0].bounds[3]
+    cut_poly1_ymax = cut_polys.geoms[1].bounds[3]
+    compress_poly = cut_polys.geoms[0] if cut_poly0_ymax > cut_poly1_ymax else cut_polys.geoms[1]
+
+    Pc = 0.85 * mat.fc * compress_poly.area        # kgf
+    Mcx = Pc * -compress_poly.centroid.y           # kgf-cm
+    Mcy = Pc * +compress_poly.centroid.x           # kgf-cm
+
+    # steel_force
+    #
+    # Yi  = strained fiber at level i
+    # Ymax  = maximum compressive fiber
+    # Y0  = “zero” strain fiber in cross-section
+    y0 = poly_ymax - c
+
+    rbs_df["epsilon_s"] = mat.eps_cu / c * (rbs_df["Yri"] - y0)
+    rbs_df["sigma_s"] = [mat.sigma_s(i) for i in rbs_df["epsilon_s"]]
+
+    dfc = []
+    for _ in rbs_df["epsilon_s"]:
+        if _ < 0:
+            dfc.append(0)
+        else:
+            dfc.append(-0.85 * mat.fc)
+
+    rbs_df["dfc"] = dfc
+    rbs_df["Ps"] = (rbs_df["sigma_s"] + rbs_df["dfc"]) * rbs_df["As"]
+    rbs_df["Msx"] = - rbs_df["Ps"] * rbs_df["Yri"]
+    rbs_df["Msy"] = + rbs_df["Ps"] * rbs_df["Xri"]
+
+    Ps = sum(rbs_df["Ps"])    # kgf
+    Msx = sum(rbs_df["Msx"])  # kgf-cm
+    Msy = sum(rbs_df["Msy"])  # kgf-cm
+
+    Pn = Pc + Ps
+    Mnx = (Mcx + Msx) * math.cos(-angle_deg * math.pi/180) + \
+          (Mcy + Msy) * math.sin(-angle_deg * math.pi/180)
+    Mny = -(Mcx + Msx) * math.sin(-angle_deg * math.pi/180) + \
+        (Mcy + Msy) * math.cos(-angle_deg * math.pi/180)
+    
+    return Pn, Mnx, Mny 
 
 def forces_moments(neutral_axis_depth):
     
@@ -186,87 +230,190 @@ def pm_curve_intersection(y_p0, x_m0, *curve_points):
     
     return y_pi, x_mi, fs
 
-nom_Axial_load = []
-phi_Axial_load = []
-nom_moment = []
-phi_moment = []
-eccentricity = []
-phi_factor = []
-epsilon_t = []
-neutral_axis_depth = []
-control_type =[]
 
 # -----------------------------------------------------------
-NUM = 20
-c_value = np.linspace(0.00001, 1.2* rec.height, NUM)
+# 斷面資料 unit:cm
+rec = RectangleSec(width=50, height=80)
+rec_poly = Polygon(rec.coordinate)
+rec_df = pd.DataFrame(list(rec_poly.exterior.coords), columns=['Xi', 'Yi'])
+
+angle_deg = 180
+
+# --------------------------------------------------------
+# 鋼筋資料
+# 以斷面形心為原點中心 
+rebars = []
+rebars.append({"area":1*Rebars.rA(22),"coord":(-19,-34)})
+rebars.append({"area":1*Rebars.rA(22),"coord":(  0,-34)})
+rebars.append({"area":1*Rebars.rA(22),"coord":( 19,-34)})
+rebars.append({"area":1*Rebars.rA(22),"coord":(-19, 34)})
+rebars.append({"area":1*Rebars.rA(22),"coord":(  0, 34)})
+rebars.append({"area":1*Rebars.rA(22),"coord":( 19, 34)})
+
+rbs = Rebars(rebars)
+rebarPoints = MultiPoint(rbs.coordinate)
+
+pt_x   = [point.x for point in rebarPoints.geoms]
+pt_y   = [point.y for point in rebarPoints.geoms]
+rb_a   = [rb["area"] for rb in rbs.rebars]
+rbs_df = pd.DataFrame(list(zip(pt_x,pt_y,rb_a)), columns=['Xi', 'Yi','As'])
+
+# -----------------------------------------------------------
+# 材料資料 unit:cm
+mat = RcMaterial(fc = 280, fy = 4200)
+
+# -----------------------------------------------------------
+# plastic-centroid for the origin of the coordinate system
+x_pc, y_pc =0, 0
 
 
-# Pure Compression
-# Nominal axial compressive strength at zero eccentricity 
-p0 = 0.85*mat.fc*(rec.area-rbs.total_area)+rbs.total_area*mat.fy
-phi_p0 = 0.8*0.65*p0
+# Concrete Coordinates and Cross-Sectional Properties
+rec_poly = geometry_transform(rec_poly, x_pc, y_pc, angle_deg)
+rec_poly_xmin = rec_poly.bounds[0]
+rec_poly_ymin = rec_poly.bounds[1]
+rec_poly_xmax = rec_poly.bounds[2]
+rec_poly_ymax = rec_poly.bounds[3]
+rec_df["Xri"] = rec_poly.exterior.coords.xy[0]
+rec_df["Yri"] = rec_poly.exterior.coords.xy[1]
+
+
+# Rebar Coordinates and Transformed Cross-Sectional Properties
+rebarPoints = geometry_transform(rebarPoints, x_pc, y_pc, angle_deg)
+rbs_df["Xri"] = [point.x for point in rebarPoints.geoms]
+rbs_df["Yri"] = [point.y for point in rebarPoints.geoms]
 
 
 
+# -----------------------------------------------------------
+# 外力資料 unit:t-m
+pu0 = 0.0
+mu0 = 1.0
 
-PM_curve_points = []
+# Make PM curve   
+NUM = 30
+c_depth = rec_poly_ymax - rec_poly_ymin
+c_value = np.linspace(0.0001, 1.0 * c_depth, NUM)
+
+pmCurve = []
+# 全斷面受拉
+# Pure Tension
+Pt = -rbs.total_area * mat.fy
+pmCurve.append({"Pn": Pt/1000, "Mnx": 0, "Mny": 0})
+
 for c in c_value:
-    # 最大拉力鋼筋應變
-    ept, fs = mat.steel_strain_stress_at_depth(c, rec.height/2-rbs.lowest)
-    phi, classify =  strength_reduction_factor(ept)
-    ecc, Pn, phi_Pn, Mn, phi_Mn = forces_moments(c)
+    Pn, Mnx, Mny = computed_capacity_force(rec_poly, c)
+    pmCurve.append({"Pn": Pn/1000, "Mnx":Mnx/100000, "Mny":Mny/100000})
 
-    nom_Axial_load.append(Pn/1000)
-    phi_Axial_load.append(phi_Pn/1000)
-    nom_moment.append(Mn/100000)
-    phi_moment.append(phi_Mn/100000)
-    eccentricity.append(ecc)
-    phi_factor.append(phi)
-    neutral_axis_depth.append(c)
-    epsilon_t.append(ept)
-    control_type.append(classify)
-    PM_curve_points.append(Point(phi_Mn/100000, phi_Pn/1000))
+# 全斷面受壓
+# Pure Compression
+Pc = 0.85*mat.fc*(rec_poly.area-rbs.total_area) + rbs.total_area * mat.fy
+pmCurve.append({"Pn": Pc/1000, "Mnx": 0, "Mny": 0}) 
+pmCurve_df = pd.DataFrame(pmCurve , columns=["Pn", "Mnx","Mny"])
+print(pmCurve_df)
 
-dict = {"Pn": nom_Axial_load,
-        "Mn": nom_moment,
-        "phiPn": phi_Axial_load,
-        "phiMn": phi_moment,
-        "ecc": eccentricity,
-        "NA_depth": neutral_axis_depth,
-        "eps_t": epsilon_t,
-        "cont.": control_type,
-        "phi": phi_factor,}
 
-df = pd.DataFrame(dict)
-
-output = df.to_string(formatters={
-    'eps_t': '{:,.4f}'.format,
-    'Pn': '{:,.3f}'.format,
-    'Mn': '{:,.3f}'.format,
-    'NA_depth': '{:,.3f}'.format,
-    'ecc': '{:,.3f}'.format,
-    'phi': '{:,.3f}'.format,
-})
-print(output)
-
-# 求求交點
-phi_pi, phi_mi, fs = pm_curve_intersection(pu0, mu0, *PM_curve_points)
-print(f'phi_Pn  = {phi_pi} tf')
-print(f'phi_Mn  = {phi_mi} tf-m')
-print(f'FS = {fs}')
-
-# -----------------------------------------------------------
-plt.figure()
-
-plt.plot(df["Mn"], df["Pn"],linestyle='-' , marker='.')
-plt.plot(df["phiMn"], df["phiPn"], linestyle='--')
+# Plot PM curve
+plt.figure() 
+plt.plot(pmCurve_df["Mnx"], pmCurve_df["Pn"],linestyle='-' , marker='.')
 plt.plot(mu0,pu0,'rx')
-
-plt.xlim(0)
 plt.title("P-M Interation Diagram")
 plt.xlabel("M (tf-m)")
 plt.ylabel("P (tf)")
 plt.legend(["Nominal strength","Design strength"]) 
 plt.grid(True)
 plt.show()
+
+# ax = plt.figure().add_subplot(projection='3d')
+# x = pmCurve_df["Mnx"]
+# y = pmCurve_df["Mny"]
+# z = pmCurve_df["Pn"]
+# ax.plot(x, y, z, label='pm curve')
+# ax.legend(["Nominal strength","Design strength"])
+# ax.grid(True) 
+# plt.show()
+
+
+# nom_Axial_load = []
+# phi_Axial_load = []
+# nom_moment = []
+# phi_moment = []
+# eccentricity = []
+# phi_factor = []
+# epsilon_t = []
+# neutral_axis_depth = []
+# control_type =[]
+
+# # -----------------------------------------------------------
+# NUM = 20
+# c_value = np.linspace(0.00001, 1.2* rec.height, NUM)
+
+
+# # Pure Compression
+# # Nominal axial compressive strength at zero eccentricity 
+# p0 = 0.85*mat.fc*(rec.area-rbs.total_area)+rbs.total_area*mat.fy
+# phi_p0 = 0.8*0.65*p0
+
+
+
+
+# PM_curve_points = []
+# for c in c_value:
+#     # 最大拉力鋼筋應變
+#     ept, fs = mat.steel_strain_stress_at_depth(c, rec.height/2-rbs.lowest)
+#     phi, classify =  strength_reduction_factor(ept)
+#     ecc, Pn, phi_Pn, Mn, phi_Mn = forces_moments(c)
+
+#     nom_Axial_load.append(Pn/1000)
+#     phi_Axial_load.append(phi_Pn/1000)
+#     nom_moment.append(Mn/100000)
+#     phi_moment.append(phi_Mn/100000)
+#     eccentricity.append(ecc)
+#     phi_factor.append(phi)
+#     neutral_axis_depth.append(c)
+#     epsilon_t.append(ept)
+#     control_type.append(classify)
+#     PM_curve_points.append(Point(phi_Mn/100000, phi_Pn/1000))
+
+# dict = {"Pn": nom_Axial_load,
+#         "Mn": nom_moment,
+#         "phiPn": phi_Axial_load,
+#         "phiMn": phi_moment,
+#         "ecc": eccentricity,
+#         "NA_depth": neutral_axis_depth,
+#         "eps_t": epsilon_t,
+#         "cont.": control_type,
+#         "phi": phi_factor,}
+
+# df = pd.DataFrame(dict)
+
+# output = df.to_string(formatters={
+#     'eps_t': '{:,.4f}'.format,
+#     'Pn': '{:,.3f}'.format,
+#     'Mn': '{:,.3f}'.format,
+#     'NA_depth': '{:,.3f}'.format,
+#     'ecc': '{:,.3f}'.format,
+#     'phi': '{:,.3f}'.format,
+# })
+# print(output)
+
+# 求求交點
+# phi_pi, phi_mi, fs = pm_curve_intersection(pu0, mu0, *PM_curve_points)
+# print(f'phi_Pn  = {phi_pi} tf')
+# print(f'phi_Mn  = {phi_mi} tf-m')
+# print(f'FS = {fs}')
+
+# -----------------------------------------------------------
+# plt.figure()
+
+# plt.plot(df["Mn"], df["Pn"],linestyle='-' , marker='.')
+# plt.plot(df["phiMn"], df["phiPn"], linestyle='--')
+# plt.plot(mu0,pu0,'rx')
+
+# plt.xlim(0)
+# plt.title("P-M Interation Diagram")
+# plt.xlabel("M (tf-m)")
+# plt.ylabel("P (tf)")
+# plt.legend(["Nominal strength","Design strength"]) 
+# plt.grid(True)
+# plt.show()
 
